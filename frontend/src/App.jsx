5e338563170cc2d25fdcae20b40dea288a3f9344
niiad <samuelbrownai@gmail.com>
2024-05-06 10:47:28 +0000

    speaker profile component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+function SpeakerProfile(props) {
+  return (
+    <>
+      <h3>{props.name}</h3>
+      <p>Position: {props.jobTitle}</p>
+      <p>Company: {props.company}</p>
+    </>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Speaker Profile</h1>
+      <SpeakerProfile
+        name="Juliet Runolf"
+        jobTitle="Director, Marketing"
+        company="Abernathy Group"
+      />
+    </>
+  );
 }
 
 export default App;
