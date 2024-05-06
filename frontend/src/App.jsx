d6981fe990b3abefa412c99626eef18760d302d7
niiad <samuelbrownai@gmail.com>
2024-05-06 14:24:58 +0000

    updated app.jsx to use useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -15,11 +15,46 @@ function SpeakerProfile(props) {
 }
 
 function App() {
+  const [data, setData] = useState([]);
+  const apiUrl = "https://dummyjson.com/users";
+
+  const fetchSpeakers = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      setData(data.users);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  let isMounted = true;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await fetchSpeakers();
+
+      if (isMounted) {
+        setData(fetchedData);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [data]);
+
   return (
-    <div style={{ backgroundColor: "blue", margin: 20, color: "white" }}>
-      <h1>Bizza Platform</h1>
-      <SearchSpeaker />
-    </div>
+    <ul>
+      {data.map((item) => (
+        <li key={item.id}>
+          {item.firstName} {item.lastName}
+        </li>
+      ))}
+    </ul>
   );
 }
 
